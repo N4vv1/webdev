435df036353b02b66d4916ffbe28e11727c3df33
N4vv1 <sgrim420@gmail.com>
2024-12-02 22:43:23 +0800

    binura ko lumang db

--- kdepot.sql
+++ kdepot.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 28, 2024 at 02:08 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Dec 02, 2024 at 03:41 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,18 +24,28 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `orders`
+-- Table structure for table `cart`
 --
 
-CREATE TABLE `orders` (
-  `order_id` int(11) NOT NULL,
-  `customer_name` varchar(255) NOT NULL,
-  `customer_email` varchar(255) NOT NULL,
-  `total_amount` decimal(10,2) NOT NULL,
-  `status` enum('Proccessing','Shipped','Delivered') NOT NULL DEFAULT 'Proccessing',
+CREATE TABLE `cart` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_name` varchar(255) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `quantity` int(11) NOT NULL DEFAULT 1,
   `payment_status` enum('Paid','Pending') NOT NULL DEFAULT 'Pending',
-  `order_date` timestamp NOT NULL DEFAULT current_timestamp()
+  `status` enum('Shipped','Delivered','Proccessing') NOT NULL DEFAULT 'Proccessing',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_id`, `user_id`, `product_id`, `product_name`, `price`, `quantity`, `payment_status`, `status`, `created_at`, `updated_at`) VALUES
+(41, 2, 13, 'Seido Knife Set', 25000.00, 1, 'Pending', 'Proccessing', '2024-12-02 14:05:05', '2024-12-02 14:05:05');
 
 -- --------------------------------------------------------
 
@@ -59,20 +69,48 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`product_id`, `product_name`, `price`, `description`, `category`, `image_url`, `stock`, `created_at`) VALUES
-(4, 'ghjfg', 3345234.00, 'dgsxdfgsfhaefgsagafesrdhjdhs', 'fhcsfgh', '../uploads/d1.PNG', 69, '2024-11-28 10:53:04'),
-(5, 'red', 123.00, '123asdjnlsfn', 'knife', '../uploads462538258_3411533022311086_3384600919916044771_n.jpg', 24, '2024-11-28 11:13:40'),
-(6, 'qwer', 10.00, 'qwertyuiop', 'knife', './uploadsd1.PNG', 12, '2024-11-28 11:15:41'),
-(7, 'red', 15.00, 'adsfdsfefesf', 'knife', 'uploadsd1.PNG', 45, '2024-11-28 11:16:19');
+(13, 'Seido Knife Set', 25000.00, 'The Seido knife set features high-quality Japanese steel knives known for sharpness, durability, and precision. With ergonomic handles and excellent edge retention, the set is ideal for both professional chefs and home cooks.', 'Cutting Tools', '../uploads/seido set.jpg', 24, '2024-12-02 13:57:33');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_info`
+--
+
+CREATE TABLE `user_info` (
+  `user_id` int(11) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `middle_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `contact_number` varchar(15) NOT NULL,
+  `address` text NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `confirm_password` varchar(255) NOT NULL,
+  `role` enum('admin','user') NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_info`
+--
+
+INSERT INTO `user_info` (`user_id`, `first_name`, `middle_name`, `last_name`, `email`, `contact_number`, `address`, `password`, `confirm_password`, `role`) VALUES
+(1, 'John', 'Paul', 'Doe', 'john.doe@example.com', '09123456789', '123 Main St, Pasig City', 'password123', 'password123', 'user'),
+(2, 'JC', 'Aira', 'Qartil', 'qartil@gmail.com', '12345678910', 'sa puso ni aira', 'aira143', '', 'user'),
+(3, 'q', '', 'q', 'asd123@gmail.com', '12345', 'qwerty', '$2y$10$6/f5XauiH5O.ct43yV/pl.2cT7021/yfQrbIOSquz63.meRCRpMFa', '', 'user'),
+(4, 'admin', 'admin', 'admin', 'admin@gmail.com', '12312312312', 'admin', 'admin', 'admin', 'admin');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `orders`
+-- Indexes for table `cart`
 --
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`order_id`);
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cart_id`),
+  ADD KEY `fk_cart_product` (`product_id`),
+  ADD KEY `fk_user_id` (`user_id`);
 
 --
 -- Indexes for table `products`
@@ -81,20 +119,44 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`product_id`);
 
 --
+-- Indexes for table `user_info`
+--
+ALTER TABLE `user_info`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `orders`
+-- AUTO_INCREMENT for table `cart`
 --
-ALTER TABLE `orders`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `cart`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `user_info`
+--
+ALTER TABLE `user_info`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `fk_cart_product` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user_info` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
